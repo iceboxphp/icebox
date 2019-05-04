0c3b3eff6be71282fa11617393b5615372cb25a4
Mahfuzur Rahman <bappy.gm@gmail.com>
2019-05-04 21:37:07 +0600

    quick refactor: src/config/initializers/activerecord.php

--- src/config/initializers/activerecord.php
+++ src/config/initializers/activerecord.php
@@ -1,6 +1,12 @@
 <?php
 
-use Icebox\ActiveRecord\Config as ArConfig;
+use ActiveRecord\Config as ArConfig;
+
+// ArConfig::initialize(function($cfg)
+// {
+//    $database_config = include __DIR__.'/../database.php';
+//    $cfg->set_connections($database_config);
+// });
 
 ArConfig::initialize(function($cfg)
 {
